8bf5bd2df6627a999afa5ce807048d6fb7370e78
neoyoun <453400438@qq.com>
2016-06-08 17:45:26 +0800

    add relativeTo attribute

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -13,7 +13,33 @@ var H5ComponentBase = function ( name, cfg) {
                     marginLeft : '-'+cfg.width/4+'px'
                 })
 	cfg.bg && component.css('backgroundImage','url('+cfg.bg+')')
+	/*
+	*获取父节点的偏移量，必须已经添加进dom结构才可以获取
+	*使用css3 的translate属性来进行偏移
+	*/
+/*	if(cfg.relativeTo){
+		var parent = $('body').find('.h5_component_name_'+cfg.relativeTo)
+		var position = {
+			left : $(parent)[0].offsetLeft,
+			top : $(parent)[0].offsetTop
+		}
+		if(cfg.center === true){
+			position.left = 0
+		}
+		component.css('transform' , 'translate('+ position.left +'px,'+ position.top +'px')
+	}*/
+	
+
 	component.on('onLoad',function () {
+		/*方法二、修改dom结构实现 relativeTo
+		*处理dom元素应该在添加进结构中后才进行操作
+		*需要添加一个自动触发 onLoad 事件的代码段
+		*/
+		if (cfg.relativeTo) {
+				var parent = component.parent().find('.h5_component_name_'+cfg.relativeTo)
+				component.appendTo(parent)
+			};
+
 		/*已有的 name className 会被忽略 下同*/
 		component.addClass(cls+'_load').removeClass(cls + '_leave')
 		cfg.animateIn && component.animate( cfg.animateIn)
