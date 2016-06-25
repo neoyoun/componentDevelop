fc0ad3e4644da888a83e79cc38cf759b225bc0f7
neoyoun <453400438@qq.com>
2016-06-25 17:05:22 +0800

    finish component-Bar_v

--- js/H5ComponentBar.js
+++ js/H5ComponentBar.js
@@ -5,14 +5,13 @@ var H5ComponentBar = function (name , cfg) {
 		$.each(cfg.data , function  (idx , item) {
 			var line = $('<div class="line line_'+idx+'"></div>')
 			var name = $('<div class="name">'+item[0]+'</div>')
-			name.css({textAlign : 'center',marginLeft : '-70px'})
 			var rate = $('<div class="rate">')
 			rate.width(cfg.width*item[1])
 			var process = $('<div class="bg"></div>')
-			process.css({'height' : '100%', 'background-color' : item[2] || '#99c0ff'})
+			process.css('background-color' , item[2] || '#99c0ff')
 			process.appendTo(rate)
 			var per = $('<div class="per">'+item[1]*100+'%</div>')
-			per.css({marginLeft : '5px',marginRight :'-30px','color':item[2] || '#99c0ff'})
+			per.css({'color':item[2] || '#99c0ff'})
 			
 			line.append(name).append(rate).append(per)
 			component.append(line)
--- js/H5ComponentBar_v.js
+++ js/H5ComponentBar_v.js
@@ -1,7 +1,19 @@
 /* 垂直柱图组件对象 */
-var H5ComponentBar_v = function (name , cfg) {
-		var component = new H5ComponentBar(name , cfg)
 
-		return component	
+var H5ComponentBar_v =function ( name, cfg ) {
+
+  //  任务二：(1) 完成 component 的初始化定义（补全 var component = ???）
+  var component =  new H5ComponentBar( name ,cfg );
+  component.find('.line').height(cfg.height/2)
+  //高度和宽度对转
+  var wStep = 100/cfg.data.length
+  component.find('.line').width(wStep+'%')
+$.each(component.find('.rate') , function  () {
+	var width = $(this).css('width')
+	$(this).height(width).width('')
+})
+//重置dom结构 由上至下变为 per -> rate -> name (原先为name->rate->per)
+component.find('.per').each(function(){$(this).prev('.rate').append($(this))})
+
+return component;
 }
-
